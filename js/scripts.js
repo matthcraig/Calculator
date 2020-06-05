00a77237a8493df10c809fd2ac1b6addedf2f016
Matthew Craig <mgcraig78@gmail.com>
2020-06-04 21:21:01 -0700

    add js to perform additional operations on numbers entered

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,22 @@ const add = function(number1, number2) {
     return number1 / number2;
   };
   
-  $(document).ready(function(){
-    $("form#add").submit(function(event){
+  $(document).ready(function() {
+    $("form#calculator").submit(function() {
       event.preventDefault();
-      const number1 = parseInt($("#add1").val());
-      const number2 = parseInt($("#add2").val());
-      const result = add(number1, number2);
+      const number1 = parseInt($("#input1").val());
+      const number2 = parseInt($("#input2").val());
+      const operator = $("input:radio[name=operator]:checked").val();
+      let result;
+      if (operator === "add") {
+        result = add(number1, number2);
+      } else if (operator === "subtract") {
+        result = subtract(number1, number2);
+      } else if (operator === "multiply") {
+        result = multiply(number1, number2);
+      } else if (operator === "divide") {
+        result = divide(number1, number2);
+      }
       $("#output").text(result);
     });
   });
